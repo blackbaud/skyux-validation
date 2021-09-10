bb5d455bf5030267682b548358888930e47aab5d
John White <750350+johnhwhite@users.noreply.github.com>
2021-09-10 11:32:45 -0400

    Eject to Angular CLI (#50)
    * Add input box to code examples (#49)
    
    * update outdated vscode settings.json
    
    * run skyux upgrade
    
    * add sky-input-box to url-validation and email-validation code examples on the Docs page
    
    * fix linting errors
    
    * Eject to Angular CLI
    
    * Update ci, dependencies
    
    * Changes based on feedback
    
    * Update public api
    
    Co-authored-by: Braden Bisping <63625452+bradenbiz@users.noreply.github.com>

---
+++ projects/validation/src/public-api.ts
@@ -0,0 +1,6 @@
+export * from './modules/email-validation/email-validation.module';
+export {SkyEmailValidationDirective as ƛ1} from './modules/email-validation/email-validation.directive';
+export * from './modules/url-validation/url-validation.module';
+export {SkyUrlValidationDirective as ƛ2} from './modules/url-validation/url-validation.directive';
+export * from './modules/validation/validation';
+export * from './modules/validators/validators';
