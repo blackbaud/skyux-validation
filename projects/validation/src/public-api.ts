57e2e991a16d12814c2d53623a313ab0adb4f60c
John White <john.white@blackbaud.me>
2021-09-10 11:21:36 -0400

    Update public api

--- projects/validation/src/public-api.ts
+++ projects/validation/src/public-api.ts
@@ -1,6 +1,6 @@
 export * from './modules/email-validation/email-validation.module';
-export * from './modules/email-validation/email-validation.directive';
+export {SkyEmailValidationDirective as ƛ1} from './modules/email-validation/email-validation.directive';
 export * from './modules/url-validation/url-validation.module';
-export * from './modules/url-validation/url-validation.directive';
+export {SkyUrlValidationDirective as ƛ2} from './modules/url-validation/url-validation.directive';
 export * from './modules/validation/validation';
 export * from './modules/validators/validators';
