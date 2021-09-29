977d06ffc4288d3412434c2b83393870e03e6e0d
Alex Kingman <alex.kingman@blackbaud.me>
2021-09-29 13:02:19 -0400

    added public api comment

--- projects/validation/src/public-api.ts
+++ projects/validation/src/public-api.ts
@@ -1,6 +1,9 @@
 export * from './modules/email-validation/email-validation.module';
-export {SkyEmailValidationDirective as λ1} from './modules/email-validation/email-validation.directive';
 export * from './modules/url-validation/url-validation.module';
-export {SkyUrlValidationDirective as λ2} from './modules/url-validation/url-validation.directive';
 export * from './modules/validation/validation';
 export * from './modules/validators/validators';
+
+// Components and directives must be exported to support Angular's "partial" Ivy compiler.
+// Obscure names are used to indicate types are not part of the public API.
+export {SkyEmailValidationDirective as λ1} from './modules/email-validation/email-validation.directive';
+export {SkyUrlValidationDirective as λ2} from './modules/url-validation/url-validation.directive';
